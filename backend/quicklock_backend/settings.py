3894106e102383c5ea31af184775382fc7e1ab51
AlbertoSandovalCSUN <alberto.sandoval.950@my.csun.edu>
2025-11-04 11:08:02 -0800

    readme and settings update

--- backend/quicklock_backend/settings.py
+++ backend/quicklock_backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-cm*27qqkj4l9+9)(@rp4j#8*!8-j7wvodxt%vk*9jy^zsj6mu$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.4.131"]
+ALLOWED_HOSTS = ["192.168.4.131", "127.0.0.1", "100.115.122.27"]
 
 
 # Application definition
@@ -74,12 +78,15 @@ WSGI_APPLICATION = 'quicklock_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "quicklock_db"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
---
+++ backend/readme.md
@@ -0,0 +1,9 @@
+# QuickLock Read Me
+Hello everyone. QuickLock read me here.
+
+## How to run app
+In the directory containing "manage.py", run
+'''python
+python manage.py runserver
+'''
+
