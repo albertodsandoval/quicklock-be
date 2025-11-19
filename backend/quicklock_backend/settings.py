f6baabe6089431d009eb3ac9c4d45cec806af1b3
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2025-11-19 15:04:04 -0800

    cors header issue fixed

--- backend/quicklock_backend/settings.py
+++ backend/quicklock_backend/settings.py
@@ -47,9 +47,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'user_auth',
     'embedded',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +77,12 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8081",
+    "http://localhost:8081",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 WSGI_APPLICATION = 'quicklock_backend.wsgi.application'
 
